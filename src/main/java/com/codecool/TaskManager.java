094cfc6df982911a99a835dd2fa3ffead2cc9cab
gyoritamas <gyoritamasnoe@gmail.com>
2025-08-28 21:48:40 +0200

    feat: add TaskManager class

---
+++ src/main/java/com/codecool/TaskManager.java
@@ -0,0 +1,10 @@
+package com.codecool;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskManager {
+    private final List<Task> tasks = new ArrayList<>();
+    private int nextId = 1;
+
+}
